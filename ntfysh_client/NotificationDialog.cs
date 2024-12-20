c8ba64f5d831ea2e393566a439e291984ced1c28
mshafer1 <2565361+mshafer1@users.noreply.github.com>
2024-12-20 15:49:42 -0600

    remove unused imports

--- ntfysh_client/NotificationDialog.cs
+++ ntfysh_client/NotificationDialog.cs
@@ -1,15 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
 using System.Runtime.InteropServices;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
 
 namespace ntfysh_client
 {
