2add8ad2d6a5d292e50b1352a2e348525c884e65
mshafer1 <2565361+mshafer1@users.noreply.github.com>
2024-12-23 13:13:35 -0600

    Make the blue a little closer to the true default

--- ntfysh_client/Themes/DefaultTheme.cs
+++ ntfysh_client/Themes/DefaultTheme.cs
@@ -6,7 +6,7 @@ namespace ntfysh_client.Themes
     {
         public override Color BackgroundColor { get => Color.White; }
         public override Color ControlBackGroundColor { get => SystemColors.ControlDark; }
-        public override Color ControlMouseOverBackgroundColor { get => Color.LightSkyBlue; }
+        public override Color ControlMouseOverBackgroundColor { get => Color.CadetBlue; }
         public override Color ForegroundColor { get => SystemColors.WindowText; }
     }
 }
